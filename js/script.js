bb67b1b24ec156a147732d1a7e177ca2a485a44a
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-03-26 18:27:02 +0000

    rgrgr

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ const sq = 4;
 
 document.getElementById("myBtn").onclick = function(){
 s = document.getElementById("myInput2").value;
-myInput2 = Number(myInput2)
-perimeter = s * sq;
+s = Number(s);
+const perimeter = s * sq;
 document.getElementById("myH3").textContent = perimeter + " cm";
 }
---
+++ js/script2.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2024 Dang All rights reserved
+//
+// Created by: Dang
+// Created on: Mar 2024
+// This file contains the JS functions for index.html
+function doMath() {
+  const length = document.getElementById("lenght-of-square").value
+  const perimeter = length * 4
+  document.getElementById("myH3").innerHTML= perimeter + "cm" 
+}
